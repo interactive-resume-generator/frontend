256ee73fe817e329ef59838deabe238dfa55c934
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-05-22 21:40:38 +0000

    basic template complete

---
+++ interactive-resume-generator-fe/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <>
+            <header className='hero__header'>
+                <h1>Test User</h1>
+                <h2>Test Title</h2>
+            </header>
+        </>
+    )
+}

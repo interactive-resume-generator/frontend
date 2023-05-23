57f14623a160af7256656bb76ef7026114103ac4
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-05-23 03:17:42 +0000

    created portfolio component, and possibly dynamic sections

--- interactive-resume-generator-fe/components/ScrollSection.js
+++ interactive-resume-generator-fe/components/ScrollSection.js
@@ -1,54 +1,71 @@
-import React, {useRef, useEffect} from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
-export default function ScrollSection() {
-    const sectionRef = useRef(null)
-    const triggerRef = useRef(null)
-
-    gsap.registerPlugin(ScrollTrigger)
-
-    useEffect(() => {
-        const pin = gsap.fromTo(sectionRef.current, {
-            translateX: 0
-        }, {
-            // increase and decrease based on section
-            translateX: "-300vw",
-            ease: "none",
-            direction: 1,
-            scrollTrigger: {
-                trigger: triggerRef.current,
-                start: "top top",
-                // 2000 px at the top of the next section
-                end: "2000 top",
-                scrub: 0.6,
-                pin: true
-            }
-        })
-
-        // kills animation when the component gets unordered
-        return () => {
-            pin.kill()
-        }
-    }, {})
-
-    return (
-        <>
-            <section className='scroll-section-outer'>
-                <div ref={triggerRef}>
-                    <div ref={sectionRef} className='scroll-section-inner'>
-                        <div className='scroll-section'>
-                            <h3>Section 1</h3>
-                        </div>
-                        <div className='scroll-section'>
-                            <h3>Section 2</h3>
-                        </div>
-                        <div className='scroll-section'>
-                            <h3>Section 3</h3>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+// import React, {useRef, useEffect} from 'react';
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+//
+// export default function ScrollSection() {
+//     const sectionRef = useRef(null)
+//     const triggerRef = useRef(null)
+//
+//     gsap.registerPlugin(ScrollTrigger)
+//
+//     useEffect(() => {
+//         const pin = gsap.fromTo(sectionRef.current, {
+//             translateX: 0
+//         }, {
+//             // increase and decrease based on section
+//             translateX: "-300vw",
+//             ease: "none",
+//             direction: 1,
+//             scrollTrigger: {
+//                 trigger: triggerRef.current,
+//                 start: "top top",
+//                 // 2000 px at the top of the next section
+//                 end: "2000 top",
+//                 scrub: 0.6,
+//                 pin: true
+//             }
+//         })
+//
+//         // kills animation when the component gets unordered
+//         return () => {
+//             pin.kill()
+//         }
+//     }, [])
+//
+//     const images = [
+//         '/Scene.png',
+//         '/Train.png'
+//     ];
+//
+//     return (
+//         <>
+//             <section className='scroll-section-outer'>
+//                 <div ref={triggerRef}>
+//                     <div ref={sectionRef} className='scroll-section-inner'>
+//                         {images.map((image, index)=>(
+//                             <>
+//                                 <div
+//                                 key={index}
+//                                 className='scroll-section'
+//                                 style={{
+//                                     backgroundImage: `${image}`,
+//                                 }}>
+//
+//                                 </div>
+//                             </>
+//                         ))}
+//                         <div className='scroll-section'>
+//                             <h3>Section 1</h3>
+//                         </div>
+//                         <div className='scroll-section'>
+//                             <h3>Section 2</h3>
+//                         </div>
+//                         <div className='scroll-section'>
+//                             <h3>Section 3</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </section>
+//         </>
+//     )
+// }
--- interactive-resume-generator-fe/pages/index.js
+++ interactive-resume-generator-fe/pages/index.js
@@ -1,62 +1,14 @@
-// import { useParallax } from "react-scroll-parallax";
-// import React, { useRef } from 'react';
-// import Image from "next/image";
-// import { gsap } from 'gsap';
-//
-// const index = () => {
-//   const target = useRef(null);
-//
-//   const train = useParallax({
-//     speed: 500,
-//     targetElement: target.current,
-//   })
-//
-//   const cloud = useParallax({
-//     speed: 200,
-//     targetElement: target.current,
-//   })
-//
-//
-//   return (
-//       <>
-//           <div ref={target} style={{
-//             backgroundImage: "url('/Scene.png')",
-//             backgroundSize: "cover",
-//             backgroundPosition: 'center',
-//             width: '3000px'
-//
-//           }} className='h-screen'>
-//               <div className='fixed top-10 left-40'>
-//                  <Image src='/Sun.png' height={120} width={120} alt='picture of object in background moving'/>
-//               </div>
-//               <div ref={train.ref} className='absolute'
-//                 style={{
-//                     top: '11vh',
-//                     left: '30vw'
-//                 }}>
-//                   <Image src="/Train.png" height={350} width={700} alt='Picture of object moving'/>
-//               </div>
-//               <div ref={cloud.ref} className="absolute top-10">
-//                   <Image src="/Cloud.png" alt='' height={200} width={1000}/>
-//               </div>
-//           </div>
-//
-//       </>
-//   )
-// }
-//
-// export default index;
-
 import React from 'react'
 import Hero from "@/components/Hero";
-import ScrollSection from "@/components/ScrollSection";
 import Footer from "@/components/Footer";
+import Portfolio from "@/components/Portfolio";
 
 function Home() {
     return (
         <>
         <Hero/>
-        <ScrollSection/>
+        {/*<ScrollSection/>*/}
+        <Portfolio/>
         <Footer/>
         </>
             )

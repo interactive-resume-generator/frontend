c99be666e0b4a4338e86940eb797b365b24b4358
James Ian Solima <32881231+jamesCodes808@users.noreply.github.com>
2023-05-23 23:05:09 -0700

    Update index.js

--- interactive-resume-generator-fe/pages/index.js
+++ interactive-resume-generator-fe/pages/index.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import Portfolio from "@/components/Portfolio";
+import Nav from "@/components/Nav";
+
 
 function Home() {
+    const [education, setEducation] = useState([]);
+    const [experience, setExperience] = useState([]);
+    const [skills, setSkills] = useState([]);
+
     return (
         <>
+        <Nav
+            setEducation={setEducation}
+            setExperience={setExperience}
+            setSkills={setSkills}
+        />
         <Hero/>
-        {/*<ScrollSection/>*/}
-        <Portfolio/>
+        <Portfolio
+            education={education}
+            experience={experience}
+            skills={skills}
+        />
         <Footer/>
         </>
             )

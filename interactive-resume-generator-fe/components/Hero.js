5d8ab9354584dae6113dd8b64375f06163c2f536
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-05-26 20:17:33 +0000

    fix

--- interactive-resume-generator-fe/components/Hero.js
+++ interactive-resume-generator-fe/components/Hero.js
@@ -39,6 +39,23 @@ export default function Hero() {
         <header className="hero__header">
           <h1>Interactive Resume Generator</h1>
           <h2>Login to start!</h2>
+          <p className=" text-gray-600 max-w-[70%]"></p>
+          <Link href="/ResumeForm">
+            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              Create Resume
+            </button>
+          </Link>
+          <div className="flex items-center justify-between max-w-[400px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-600 p-6 m-6 cursor-pointer hover:scale-110 ease-in duration-300 social-bubble">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-600 p-6 m-6 cursor-pointer hover:scale-110 ease-in duration-300 social-bubble">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-600 p-6 m-6 cursor-pointer hover:scale-110 ease-in duration-300 social-bubble">
+              <FaEnvelope />
+            </div>
+          </div>
           <p> &darr; Scroll &darr;</p>
         </header>
       )}
